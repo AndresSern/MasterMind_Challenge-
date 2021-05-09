84f06b260b2e96c9b09cb8c30106ec52d804e2cb
AndresSern <andrecamposer@gmail.com>
2021-05-09 17:59:50 +0000

    Adding

--- public/src/components/menu.js
+++ public/src/components/menu.js
@@ -3,8 +3,10 @@ let rules = document.querySelector('.rules');
 let content = document.querySelector('.content');
 let close = document.querySelector('.close');
 let submit = document.querySelector('.submit');
-let code_breaker = document.querySelector('.code_breaker');
-
+let onevsbot = document.querySelector('.onevsbot');
+let onevsone = document.querySelector('.onevsone');
+let create_a_room = document.querySelector('.create_a_room');
+let join_a_room = document.querySelector('.join_a_room');
 
 start.addEventListener('click', ()=> {
 	start.style.visibility = "hidden";
@@ -12,8 +14,8 @@ start.addEventListener('click', ()=> {
 	content.style.visibility = "visible";
 	submit.style.visibility = "visible";
 	submit.style.transition = ".5s ease-out";
-	code_breaker.style.visibility = "visible";
-	code_breaker.style.transition = ".5s ease-out";
+	onevsbot.style.visibility = "visible";
+	onevsbot.style.transition = ".5s ease-out";
 });
 
 close.addEventListener('click', ()=> {
@@ -22,6 +24,18 @@ close.addEventListener('click', ()=> {
 	content.style.visibility = "hidden";
 	submit.style.visibility = "hidden";
 	submit.style.transition = "none";
-	code_breaker.style.visibility = "hidden";
-	code_breaker.style.transition = "none";
+	onevsone.style.transition = "none";
+	onevsbot.style.visibility = "hidden";
+	onevsbot.style.transition = "none";
+	onevsone.style.display = "block";
+	onevsbot.style.display = "block";
+	join_a_room.style.display = "none";
+	create_a_room.style.display = "none";
+});
+
+onevsone.addEventListener('click', ()=> {
+	onevsone.style.display = "none";
+	onevsbot.style.display = "none";
+	join_a_room.style.display = "block";
+	create_a_room.style.display = "block";
 });
